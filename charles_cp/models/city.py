44e4d1d13dea12cf0aaf6e550f41fa92d86cbb8d
Void-Individual <otayomitv@gmail.com>
2024-02-12 00:05:35 +0100

    i emptied the main branch

--- charles_cp/models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines the City class."""
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """Represents a city."""
-    state_id = ""
-    name = ""
--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-
-"""Init module for the models package"""
-
-
-storage = FileStorage()
-storage.reload()

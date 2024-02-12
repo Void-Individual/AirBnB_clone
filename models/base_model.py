4ed7ecc133aed873e5983463e50de94339b40d05
Void-Individual <otayomitv@gmail.com>
2024-02-12 02:24:27 +0100

    updated the basemodel save method

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
     def save(self):
         """Updating instance attribute 'updated_at' with current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return dictionary representation of BaseModel instance."""
